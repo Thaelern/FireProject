cafda9d7e2ab7f0ae7c52c5359080163d0296a67
Norwaii <90395745+Norwaii@users.noreply.github.com>
2022-03-28 09:35:37 +0200

    KevinBranch
    Ai behaoir and player health

---
+++ Assets/Scripts/KevinScripts/Player/PlayerHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    private float Health = 0f;
+    [SerializeField] private float maxHealth = 100f;
+
+
+    private void Start()
+    {
+        Health = maxHealth;
+    }
+
+    public void UpdateHealth(float mod)
+    {
+        Health += mod;
+
+        if (Health > maxHealth)
+        {
+            Health = maxHealth;
+        } 
+        else if (Health <= 0f)
+        {
+            Health = 0f;
+            Debug.Log("Player Respawn");
+     
+        }
+
+
+    }
+
+
+    private void Update()
+    {
+        
+
+
+        if (Health <= 0f)
+        {
+            Die();
+        }
+
+
+    }
+
+    public void Die()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+
+}
