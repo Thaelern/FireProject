84eb1ea4064bccf453d41fe32765c886137a7452
Lasse Macbook <lassebi92@gmail.com>
2022-03-29 15:01:50 +0200

    Camera
    Made the camera move side to side. When iddle, moves back to center.

--- Assets/Scripts/Camera_ChangeBehaviour_Script.cs
+++ Assets/Scripts/Camera_ChangeBehaviour_Script.cs
@@ -11,30 +11,64 @@ public class Camera_ChangeBehaviour_Script : MonoBehaviour
 
 
 
-    public GameObject cameraFollow_East;
-    public GameObject cameraFollow_West;
-    public GameObject CameraFollow_Down;
+    public GameObject cameraFollow;
+    public Transform playerController_trsform;
+    private Vector2 player_gOjt;
+
+    public float maxRangeCam;
+    public float cameraFollowSpeed;
+
+    public float cameraResetDistancePerFrame;
+
+    private void Update()
+    {
+        // current position of the player transform
+        player_gOjt = playerController_trsform.position;
+    }
 
 
-
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CameraChanger"))
         {
             cameraViewDistance.m_Lens.OrthographicSize = 10;
-            cameraViewDistance.Follow = CameraFollow_Down.transform;
+            //cameraViewDistance.Follow = CameraFollow_Down.transform;
             Invoke("DefaultCameraZoom", ResetCameraTimer);
 
         }
     }
+    */
 
-
-
-    private void DefaultCameraZoom()
+    public void CameraFollowRightFunc()
     {
-        cameraViewDistance.m_Lens.OrthographicSize = 5;
-        cameraViewDistance.Follow = cameraFollow_East.transform;
+        // limit range the camera can move player transform + 3 units ??
+        // 1st, get player.pos.x 2nd. get cameraFollow pos. 3rd max range those 2 + 3
 
+        // max travel range
+        if(cameraFollow.transform.position.x <= player_gOjt.x + maxRangeCam)
+        {
+            cameraFollow.transform.Translate(Vector3.right * Time.deltaTime * cameraFollowSpeed);
+        }
     }
+
+    public void CameraFollowLeftFunc()
+    {
+        if (cameraFollow.transform.position.x >= player_gOjt.x + -maxRangeCam)
+        {
+            cameraFollow.transform.Translate(-Vector3.right * Time.deltaTime * cameraFollowSpeed);
+        }
+    }
+
+    public void cameraFollowReturnCenter()
+    {
+        if(cameraFollow.transform.position.x != player_gOjt.x)
+        {
+            Debug.Log("Test");
+            cameraFollow.transform.position = Vector2.MoveTowards(cameraFollow.transform.position, playerController_trsform.position,cameraResetDistancePerFrame * Time.deltaTime)  ;
+        }
+    }
+
+
 
 }
