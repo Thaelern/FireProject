843b708ee130927df27a53f008391ddd1b66bfd9
Vetle Viggen <Jasindu@hotmail.com>
2022-04-21 09:39:37 +0200

    upload from vetle

---
+++ Assets/Scripts/KevinScripts/Enemy2/Enemy5.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy5 : MonoBehaviour
+{
+
+    public float walkSpeed;
+    public float distance;
+
+    public bool mustTurn;
+
+    public Rigidbody2D rb;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+
+
+    public void Update()
+    {
+        
+        if ((bodyCollider.IsTouchingLayers(groundLayer) == true))
+        {
+            Flip();
+        }
+
+        {
+            transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+            if (mustTurn == false)
+                if (mustTurn == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    mustTurn = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+
+                    mustTurn = true;
+
+                }
+        }
+
+
+    }
+       
+    void Flip()
+    {
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustTurn = false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
